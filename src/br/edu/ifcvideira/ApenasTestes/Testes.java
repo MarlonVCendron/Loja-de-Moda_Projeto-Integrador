b3c688cc7c4af56725fefcfc9da26f631a8f0637
MarlonVCendron <38591551+MarlonVCendron@users.noreply.github.com>
2018-10-14 20:05:00 -0300

    Mudando o texto apenas para testar

--- src/br/edu/ifcvideira/ApenasTestes/Testes.java
+++ src/br/edu/ifcvideira/ApenasTestes/Testes.java
@@ -2,6 +2,6 @@ package br.edu.ifcvideira.ApenasTestes;
 
 public class Testes {
 	public static void main(String[] args) {
-		System.out.println("Teste");
+		System.out.println("Testando de novo");
 	}
 }
