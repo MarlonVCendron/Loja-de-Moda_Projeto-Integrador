5853273858c6e00f75fc32865a2a56c43d0dfb54
MarlonVCendron <marlonvcendron@gmail.com>
2018-10-15 20:21:26 -0300

    Deletando o pacote de testes

--- src/br/edu/ifcvideira/ApenasTestes/Testes.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifcvideira.ApenasTestes;
-
-public class Testes {
-	public static void main(String[] args) {
-		//oloco bisho
-		System.out.println("Testando de novo");
-	}
-}
-//oloco
