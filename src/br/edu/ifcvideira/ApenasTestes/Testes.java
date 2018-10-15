fa037bb4b33e9624968f69f285ef0e82a8771747
theluanz <luanzuffo.lz@gmail.com>
2018-10-14 21:59:10 -0300

    Alguma coisa

--- src/br/edu/ifcvideira/ApenasTestes/Testes.java
+++ src/br/edu/ifcvideira/ApenasTestes/Testes.java
@@ -2,8 +2,8 @@ package br.edu.ifcvideira.ApenasTestes;
 
 public class Testes {
 	public static void main(String[] args) {
-		//oloco bisho
 		System.out.println("Testando de novo");
 	}
+	//safsdfadfasdf
 }
 //oloco
