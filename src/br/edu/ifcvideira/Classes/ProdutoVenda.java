aabd13a81062167fbfd7638d1a789799b75eb01c
MarlonVCendron <marlonvcendron@gmail.com>
2018-10-18 14:44:50 -0300

    Adicionando classes Usuario, Venda e ProdutoVenda

---
+++ src/br/edu/ifcvideira/Classes/ProdutoVenda.java
@@ -0,0 +1,41 @@
+package br.edu.ifcvideira.Classes;
+
+public class ProdutoVenda {
+	private int id_produtos_venda;
+	private int id_produto_produtos_venda;
+	private int id_venda_produtos_venda;
+	private double valor_unitario_produtos_venda;
+	private int qtde_produtos_venda;
+	public int getId_produtos_venda() {
+		return id_produtos_venda;
+	}
+	public void setId_produtos_venda(int id_produtos_venda) {
+		this.id_produtos_venda = id_produtos_venda;
+	}
+	public int getId_produto_produtos_venda() {
+		return id_produto_produtos_venda;
+	}
+	public void setId_produto_produtos_venda(int id_produto_produtos_venda) {
+		this.id_produto_produtos_venda = id_produto_produtos_venda;
+	}
+	public int getId_venda_produtos_venda() {
+		return id_venda_produtos_venda;
+	}
+	public void setId_venda_produtos_venda(int id_venda_produtos_venda) {
+		this.id_venda_produtos_venda = id_venda_produtos_venda;
+	}
+	public double getValor_unitario_produtos_venda() {
+		return valor_unitario_produtos_venda;
+	}
+	public void setValor_unitario_produtos_venda(double valor_unitario_produtos_venda) {
+		this.valor_unitario_produtos_venda = valor_unitario_produtos_venda;
+	}
+	public int getQtde_produtos_venda() {
+		return qtde_produtos_venda;
+	}
+	public void setQtde_produtos_venda(int qtde_produtos_venda) {
+		this.qtde_produtos_venda = qtde_produtos_venda;
+	}
+	
+	
+}
